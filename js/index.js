a3af128e274f03a19fd63ff2072337251bbd69f8
Fer Varela <varela.ff5@gmail.com>
2023-06-05 00:34:33 -0300

    agregado de venta de tickets

--- js/index.js
+++ js/index.js
@@ -7,15 +7,20 @@ const precio =200;
 function calcular(){
 let descuento=0;
 
+    switch (categoria.value) {
+        case "estudiante":
+          descuento = 0.8;
+          break;
+        case "trainee":
+          descuento=0.5;
+          break;
+        case "junior":
+          descuento=0.15;
+          break;
+        
+      }
 
-if (categoria.value === "estudiante"){
-    descuento=0.8;
-}else if (categoria === "trainee"){
-    descuento=0.5;
-}else if (categoria === "junior")
-    descuento=0.15;
 
-    
-totalAPagar.innerHTML=parseInt(precio*cant.value*(1-descuento))+1;
+totalAPagar.innerHTML="Total a pagar: $" +Math.round((precio*cant.value*(1-descuento)));
 
 }
